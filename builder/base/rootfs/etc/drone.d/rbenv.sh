35ec9ab55979bbd491e9de722d0ac7adac1cffda
Rafael Garcia <rafael.garcia@clever.com>
2016-10-12 18:16:33 +0000

    update node/npm version in base image

--- builder/base/rootfs/etc/drone.d/rbenv.sh
+++ builder/base/rootfs/etc/drone.d/rbenv.sh
@@ -1,9 +1,9 @@
-export PATH="/home/ubuntu/.rbenv/bin:$PATH"
+#export PATH="/home/ubuntu/.rbenv/bin:$PATH"
 
 # set default env vars
-export RBENV_VERSION=2.0.0-p353
-export RAILS_ENV=${RAILS_ENV:-test}
+#export RBENV_VERSION=2.0.0-p353
+#export RAILS_ENV=${RAILS_ENV:-test}
 
 # initialize and set ruby version
-eval "$(rbenv init -)"
-rbenv global 2.0.0-p353
+#eval "$(rbenv init -)"
+#rbenv global 2.0.0-p353
--- builder/base/scripts/golang.sh
+++ builder/base/scripts/golang.sh
@@ -7,9 +7,9 @@ pushd /tmp
 sudo apt-get -y install pkg-config
 
 # download and install Go binaries
-wget https://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz --quiet
-sudo tar -C /usr/lib -xzf go1.1.2.linux-amd64.tar.gz
-rm go1.1.2.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz --quiet
+sudo tar -C /usr/lib -xzf go1.7.1.linux-amd64.tar.gz
+rm go1.7.1.linux-amd64.tar.gz
 
 # ubuntu user must have access to the go
 # standard library
